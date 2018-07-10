cf658b86d188b503e2fb5f7ab4bba0023f8701a8
powellj6 <jpowell@boomtownroi.com>
2018-07-10 10:51:48 -0400

    Adding unit test project skeleton and updating gitignore.

---
+++ SugarRush/SugarRushTests/TextReplacementTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SugarRushTests
+{
+    [TestClass]
+    public class TextReplacementTests
+    {
+        [TestMethod]
+        public void ShouldUpdateHintPathVersionsInCsProjFile()
+        {
+
+        }
+
+        [TestMethod]
+        public void ShouldUpdatePackageVersionInPackagesConfig()
+        {
+
+        }
+
+        [TestMethod]
+        public void ShouldUpdateVersionInRefreshFile()
+        {
+
+        }
+    }
+}
