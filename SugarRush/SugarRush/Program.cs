f85c5bd8abe21266d366b5f11bcf7bdb11f685a6
powellj6 <jpowell@boomtownroi.com>
2018-07-25 13:13:12 -0400

    Casting of package.assemblyreferences not always possible so program downloads everything locally, then grabs the references from there.

--- SugarRush/SugarRush/Program.cs
+++ SugarRush/SugarRush/Program.cs
@@ -12,11 +12,12 @@ namespace SugarRush
     class Program
     {
         //TODO: Don't update/save file if nothing has changed
-
+        public static string _packagesFolder = @"C:\Temp\NugetPackages\";
         static void Main(string[] args)
         {
             try
             {
+                Logc("Getting configuration...");
                 var config = SugarRushHandler.GetConfiguration();
 
                 if (config.IsInvalid)
@@ -25,35 +26,47 @@ namespace SugarRush
                     return;
                 }
 
+                Logc("Getting nuget package...");
                 var package = SugarRushHandler.GetPackage(config);
 
                 if (package == null)
                 {
-                    Logc("Could not find any packages by packageID: " + config.packageID);
+                    Logc("Could not find any nuget package by packageID: " + config.packageID);
                     return;
                 }
 
+                Logc("Getting csproj files...");
                 var projFiles = SugarRushHandler.FilterFiles(SugarRushHandler.GetCsProjFiles(config.folderPath), config.exclusionPaths);
+                Logc("Getting Refresh files...");
                 var refreshFiles = SugarRushHandler.FilterFiles(SugarRushHandler.GetRefreshFiles(config.folderPath), config.exclusionPaths);
+                Logc("Getting package.config files...");
                 var packageFiles = SugarRushHandler.FilterFiles(SugarRushHandler.GetPackageFiles(config.folderPath), config.exclusionPaths);
 
-                var assReferences = package.AssemblyReferences.Select(x => (PhysicalPackageAssemblyReference) x);
+                DownloadNugetPackage(package, config.nugetRepoUrl);
 
-                var assDic = assReferences.Select(x => AssemblyName.GetAssemblyName(x.SourcePath)).ToDictionary(k => k.Name, v => v);
+                var dir = Path.GetFullPath($"{_packagesFolder}\\{package.GetFullName().Replace(' ', '.')}");
+
+                //TODO: Not really using anything in the assembly besides "FullName".
+                //Can probably just have Dictionary<dll name, FullName>
+                var assDic = package.AssemblyReferences
+                    .Select(ar => AssemblyName.GetAssemblyName($"{dir}\\{ar.Path}"))
+                    .ToDictionary(k => k.Name, v => v);
 
                 Parallel.ForEach<FileInfo>(projFiles, pf =>
                 {
-                    Console.WriteLine("Updating file: " + pf.Name);
+                    Console.WriteLine("Updating file: " + pf.FullName);
                     var doc = SugarRushHandler.GetXmlDoc(pf.FullName);
                     doc.UpdateCsProjFile(config.packageID + "." + config.packageVersion, assDic);
                 });
 
                 Parallel.ForEach<FileInfo>(refreshFiles, rf =>
                 {
+                    Console.WriteLine("Updating file: " + rf.FullName);
                     rf.UpdateRefreshFile(config.packageID + "." + config.packageVersion, assDic);
                 });
 
                 Parallel.ForEach<FileInfo>(packageFiles, pf => {
+                    Console.WriteLine("Updating file: " + pf.FullName);
                     var doc = SugarRushHandler.GetXmlDoc(pf.FullName);
                     doc.UpdatePackageConfig(config.packageID, config.packageVersion);
                 });
@@ -63,6 +76,15 @@ namespace SugarRush
             {
                 Logc(String.Format("Something went wrong: Message: {0}, StackTrace: {1} ", exc.Message, exc.StackTrace));
             }
+        }
+
+        private static void DownloadNugetPackage(IPackage package, string repoUrl)
+        {
+            IPackageRepository packageRepository = PackageRepositoryFactory.Default.CreateRepository(repoUrl);
+            PackageManager packageManager = new PackageManager(packageRepository, _packagesFolder);
+            
+
+            packageManager.InstallPackage(package, true, true, true);
         }
 
         private static void Logc(string message)
